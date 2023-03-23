341031373dc4c3b0c4902e6999631031a754e220
LuciusGabriel <lucius_gabriel@hotmail.com>
2023-03-23 18:17:02 -0300

    Inicia Lógica click

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+const unRead = document.querySelectorAll('.unread')
+const numberNotification = unRead.length
+
+const placeNumberNotification = document.querySelector('.number-notification')
+
+placeNumberNotification.textContent = numberNotification;
+
+unRead.forEach((item)=>{
+    item.addEventListener('click', ()=>{
+        item.classList.remove('unread')
+    })
+})
+
