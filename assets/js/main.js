0f27e5ce90acc0821a0dafc0bc49d4e1d7811a74
LuciusGabriel <lucius_gabriel@hotmail.com>
2023-03-27 15:42:32 -0300

    Ajustes e adequação projeot

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,13 +2,15 @@ const unRead = document.querySelectorAll('.unread')
 const numberNotification = unRead.length
 
 const placeNumberNotification = document.querySelector('.number-notification')
-
 placeNumberNotification.textContent = numberNotification;
+
+
 
 unRead.forEach((item)=>{
     item.addEventListener('click', ()=>{
         item.classList.remove('unread')
     })
+    
 })
 
 
@@ -16,6 +18,6 @@ const allRead = document.querySelector('.all-read')
 
 
 allRead.addEventListener('click', ()=>{
-    console.log('clicou')
+    
 })
 
